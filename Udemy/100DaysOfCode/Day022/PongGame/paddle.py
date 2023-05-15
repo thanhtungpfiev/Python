b04928f63f5272c4b8c5a332d6d08e73b867d337
tung.daothanh <tung.daothanh@vn.bosch.com>
2023-05-15 16:26:12 +0700

    Udemy Python course day022

---
+++ Udemy/100DaysOfCode/Day022/PongGame/paddle.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
