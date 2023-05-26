c349ef762a207e61c85188f61ae2a77c9750afe8
thanhtungpfiev <thanhtungpfiev@gmail.com>
2023-05-26 13:57:15 +0700

    Udemy Python course day033 v4

--- Udemy/100DaysOfCode/Day033/SunsetTimes/main.py
+++ Udemy/100DaysOfCode/Day033/SunsetTimes/main.py
@@ -1,9 +1,16 @@
 import requests
 
 params = {
-    "lat": 21.027763,
-    "lng": 105.834160
+    'lat': 21.027763,
+    'lng': 105.834160,
+    'formatted': 0
 }
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+response = requests.get(url='https://api.sunrise-sunset.org/json', params=params)
 response.raise_for_status()
+data = response.json()
+
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+
+print(sunrise, sunset)
--- Udemy/100DaysOfCode/Day033/issoverhead-start/main.py
+++ Udemy/100DaysOfCode/Day033/issoverhead-start/main.py
@@ -1,19 +1,26 @@
+import time
 from datetime import datetime
+import smtplib
 
 import requests
 
 MY_LAT = 51.507351  # Your latitude
 MY_LONG = -0.127758  # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+def check_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-# Your position is within +5 or -5 degrees of the ISS position.
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    # Your position is within +5 or -5 degrees of the ISS position.
+    return abs(MY_LAT - iss_latitude) <= 5 and abs(MY_LONG - iss_longitude) <= 5
 
+
+my_email = "cuongphong2508@gmail.com"
+password = "aatdvndewyaanawc"
 
 parameters = {
     "lat": MY_LAT,
@@ -21,15 +28,29 @@ parameters = {
     "formatted": 0,
 }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+def check_dark():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    return time_now.hour >= sunset or time_now.hour <= sunrise
+
 
 # If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
+
+
+while True:
+    if check_position() and check_dark():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                                msg="Subject:Lookup\n\nThe ISS is above you in your sky.")
+    time.sleep(60)
