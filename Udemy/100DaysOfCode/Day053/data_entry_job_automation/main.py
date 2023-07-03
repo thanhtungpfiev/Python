762f2a386a0435d5f6f54abb4df48c973e298faa
thanhtungpfiev <thanhtungpfiev@gmail.com>
2023-07-03 10:02:47 +0700

    Udemy Python course day054

--- Udemy/100DaysOfCode/Day053/data_entry_job_automation/main.py
+++ Udemy/100DaysOfCode/Day053/data_entry_job_automation/main.py
@@ -8,12 +8,22 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 header = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/84.0.4147.125 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
 response = requests.get(
-    "https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
+    "https://www.zillow.com/homes/San-Francisco,"
+    "-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C"
+    "%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37"
+    ".69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C"
+    "%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue"
+    "%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value"
+    "%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A"
+    "%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp"
+    "%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C"
+    "%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
     headers=header)
 
 data = response.text
@@ -47,7 +57,8 @@ service = Service(executable_path=CHROME_DRIVER_PATH)
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-URL_TO_YOUR_GOOGLE_FORM = "https://docs.google.com/forms/d/e/1FAIpQLSc-TWQbycf8orimW7iLrHEU9umvE2CYMfGStgTPk9FMM13ycw/viewform?usp=sf_link"
+URL_TO_YOUR_GOOGLE_FORM = "https://docs.google.com/forms/d/e/1FAIpQLSc-TWQbycf8orimW7iLrHEU9umvE2CYMfGStgTPk9FMM13ycw" \
+                          "/viewform?usp=sf_link"
 
 for n in range(len(all_links)):
     # Substitute your own Google Form URL here 👇
@@ -55,7 +66,8 @@ for n in range(len(all_links)):
 
     time.sleep(2)
     address = driver.find_element(By.XPATH,
-                                  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+                                  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div['
+                                  '1]/input')
     price = driver.find_element(By.XPATH,
                                 '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
     link = driver.find_element(By.XPATH,
