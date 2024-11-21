1cbc2b7068e9c43d1074ee61003ca00e0df9b144
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-21 16:45:48 +0700

    Learning PySide6 Organize your code in classes v2

--- Learning/Youtube/LearnQtGuide/PySide6/01-EnvironmentSetup/03-OrganizingCodeInClasses/main.py
+++ Learning/Youtube/LearnQtGuide/PySide6/01-EnvironmentSetup/03-OrganizingCodeInClasses/main.py
@@ -1,17 +1,23 @@
-#VERSION1: Setting everything up in the global scope
+#VERSION2: Setting up a separate class
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
 
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Our first MainWindow App!")
+
+        button = QPushButton()
+        button.setText("Press Me")
+
+        self.setCentralWidget(button)
+
+
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle("Our first MainWindow App!")
-
-button = QPushButton()
-button.setText("Press Me")
-
-window.setCentralWidget(button)
-
+window = ButtonHolder()
 window.show()
+
 app.exec()
 
